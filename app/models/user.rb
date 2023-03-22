4c6af550d7cc632c362468fdbcdebc12e1a93827
KayT <126052068+KayTsujita@users.noreply.github.com>
2023-03-22 19:30:30 +0900

    ユーザー新規登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birth_date, presence: true
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: ' is invalid. Input full-width characters'}
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: ' is invalid. Input full-width katakana characters'}
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: ' is invalid. Include both letters and numbers' }
+
+
 end
