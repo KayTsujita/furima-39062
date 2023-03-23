69662b5ea814fc09064c089463f10363c4d9930a
KayT <126052068+KayTsujita@users.noreply.github.com>
2023-03-23 16:37:38 +0900

    validation修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birth_date, presence: true
   validates :last_name, :first_name, presence: true,
-                                     format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' }
+                                     format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
   validates :last_name_kana, :first_name_kana, presence: true,
                                                format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
   validates :password,
