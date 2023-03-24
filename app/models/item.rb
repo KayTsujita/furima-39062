3c7fb3855b6a68b25d8bd5f7bfc3708ab7853bef
KayT <126052068+KayTsujita@users.noreply.github.com>
2023-03-24 12:16:33 +0900

    プルダウン設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :delivery
 
   validates :item_name, presence: true
   validates :description, presence: true
@@ -11,4 +17,5 @@ class Item < ApplicationRecord
   validates :delivery_time_id, presence: true
   validates :price, presence: true
   validates :image, presence: true
+  validates :category_id,:condition_id, :postage_id, :prefecture_id, :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
 end
---
+++ app/models/postage.rb
@@ -0,0 +1,11 @@
+class Postage < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '送料込み' },
+    { id: 3, name: '着払い' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+  
+end
