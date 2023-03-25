be829120fa72304434e5f7a29fc4933aa0323abf
KayT <126052068+KayTsujita@users.noreply.github.com>
2023-03-25 15:47:14 +0900

    単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,15 +8,17 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery
 
-  validates :item_name, presence: true
-  validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :postage_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_time_id, presence: true
-  validates :price, presence: true
-  validates :image, presence: true
+  with_options presence: true do
+  validates :image
+  validates :item_name
+  validates :description
+  validates :category_id
+  validates :condition_id
+  validates :postage_id
+  validates :prefecture_id
+  validates :delivery_time_id
   validates :category_id,:condition_id, :postage_id, :prefecture_id, :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters" }
+  end
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range"}
 end
