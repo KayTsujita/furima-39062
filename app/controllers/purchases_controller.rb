9c89beadc40150cf50c07fc6d0ef6242b949d066
KayT <126052068+KayTsujita@users.noreply.github.com>
2023-04-03 21:54:38 +0900

    購入画面作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,26 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  
   def index
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
   end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid? 
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postcode, :prefecture_id, :city, :address, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
