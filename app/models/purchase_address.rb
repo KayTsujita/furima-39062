2a7196007d5ef33e7f70c52e2a90f28e6eed676d
KayT <126052068+KayTsujita@users.noreply.github.com>
2023-04-03 21:53:52 +0900

    Formオブジェクト設定

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,20 @@
+class PurchaseAddress
+
+  include ActiveModel::Model
+  attr_accessor :postcode, :prefecture_id, :city, :address, :building, :phone, :user_id, :item_id
+
+
+  with_options presence: true do
+    validates :user_id
+    validates :postcode,          format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :prefecture_id,     numericality: {other_than: 1, message: "can't be blank"}
+    validates :city              
+    validates :address           
+    validates :phone,             format: {with: /\A\d{11}\z/, message: "is invalid. Input only number"}
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, address: address, building: building, phone: phone, purchase_id: purchase.id)
+  end
+end
