d8d46408ac13288808f30f919228628d15b39928
KayT <126052068+KayTsujita@users.noreply.github.com>
2023-04-10 17:39:19 +0900

    deploy.rb記述修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,12 @@
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.2"
-
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'capistrano (3.17.2)'
-
 # Capistranoのログの表示に利用する
-set :application, 'furima-39062'
-
+set :application, "furima-39062"
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:KayTsujita/furima-39062.git'
+set :repo_url, 'git@github.com:KayTsujita/furima-39062.git'
+
+
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
